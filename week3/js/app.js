e1f48be5985c84a3b34630e075f7168b41ea3aba
Touch Nguonchhay <nguonchhay.touch@gmail.com>
2024-05-10 19:31:30 +0700

    [TASK] Week 3 complete todo action

--- week3/js/app.js
+++ week3/js/app.js
@@ -12,6 +12,48 @@ const writeToDoData = toDoList => {
     localStorage.setItem('todo', JSON.stringify(toDoList));
 }
 
+const loadDataToToDoTable = () => {
+    const toDoList = readToDoData();
+    for (toDoItem of toDoList) {
+        addNewRowToTable(toDoItem);
+        todoId = toDoItem.id;
+    }
+    todoId++;
+}
+
+const closeModal = modalId => {
+    var myModalEl = document.getElementById(modalId);
+    var modal = bootstrap.Modal.getInstance(myModalEl);
+    modal.hide();
+}
+
+const openModal = modalId => {
+    var myModalEl = document.getElementById(modalId);
+    var modal = new bootstrap.Modal(myModalEl);
+    modal.show();
+}
+
+const displayEditToDoItemForm = toDoItem => {
+    document.getElementById('editId').value = toDoItem.id;
+    document.getElementById('editTitle').value = toDoItem.title;
+    document.getElementById('editDueDate').value = toDoItem.dueDate;
+    document.getElementById('editStatus').value = toDoItem.status;
+    document.getElementById('btnEditToDo').setAttribute('value', toDoItem.id);
+    openModal('modalEditToDo');
+}
+
+const removeRowFromTable = selectedId => {
+    const todoData = readToDoData();
+    /**
+     * const remainToDo = todoData.filter((todo) => {
+     *  return todo.id !== selectedId;
+     * });
+     */
+    const remainToDo = todoData.filter(todo => todo.id !== selectedId);
+    writeToDoData(remainToDo);
+    window.location.reload();
+}
+
 const addNewRowToTable = toDoItem => {
     const tableToDo = document.getElementById('tableToDo');
 
@@ -32,11 +74,39 @@ const addNewRowToTable = toDoItem => {
     let tdStatus = document.createElement('td');
     tdStatus.innerHTML = toDoItem.status;
 
+    let tdAction = document.createElement('td');
+
+    let buttonGroup = document.createElement('div');
+    buttonGroup.className = 'btn-group';
+    buttonGroup.setAttribute('role', 'group');
+
+    let btnEdit = document.createElement('button');
+    btnEdit.className = 'btn btn-primary';
+    btnEdit.innerHTML = 'Edit';
+    btnEdit.onclick = () => {
+        displayEditToDoItemForm(toDoItem);
+    }
+
+    let btnDelete = document.createElement('button');
+    btnDelete.className = 'btn btn-danger';
+    btnDelete.innerHTML = 'Delete';
+    btnDelete.onclick = () => {
+        if (confirm('Are you sure?')) {
+            removeRowFromTable(toDoItem.id);
+        }
+    }
+
+    buttonGroup.appendChild(btnEdit);
+    buttonGroup.appendChild(btnDelete);
+    tdAction.appendChild(buttonGroup);
+
+
     // Append cells to row
     row.appendChild(tdIndex);
     row.appendChild(tdTitle);
     row.appendChild(tdDueDate);
     row.appendChild(tdStatus);
+    row.appendChild(tdAction);
 
     // Append row to table body
     tableToDo.appendChild(row);
@@ -63,10 +133,36 @@ const saveToDoItem = todoItem => {
     todoId++;
 }
 
-const closeModal = modalId => {
-    var myModalEl = document.getElementById(modalId);
-    var modal = bootstrap.Modal.getInstance(myModalEl)
-    modal.hide();
+const updateToDoItem = todoItem => {
+    const toDoList = readToDoData();
+    let updateToDoList = [];
+    for (item of toDoList) {
+        if (item.id == todoItem.id) {
+            updateToDoList.push(todoItem);
+        } else {
+            updateToDoList.push(item);
+        }
+    }
+    writeToDoData(updateToDoList);
+    window.location.reload();
+}
+
+function editToDoItem(editButton) {
+    const elementTitle = document.getElementById('editTitle');
+    const elementDueDate = document.getElementById('editDueDate');
+    const elementStatus = document.getElementById('editStatus');
+    if (elementTitle.value === '' || elementDueDate.value === '' || elementStatus.value === '') {
+        alert('Please fill in all mandatory fields!');
+    } else {
+        const selectedId = editButton.value;
+        const todoItem = constructToDoItem(
+            elementTitle.value, 
+            elementDueDate.value, 
+            elementStatus.value, 
+            selectedId
+        );
+        updateToDoItem(todoItem);
+    }
 }
 
 const clearFormNewToDo = () => {
@@ -95,17 +191,6 @@ const createToDoItem = () => {
         addNewRowToTable(todoItem);
         closeModal('modalNewToDo');
     }
-}
-
-
-
-const loadDataToToDoTable = () => {
-    const toDoList = readToDoData();
-    for (toDoItem of toDoList) {
-        addNewRowToTable(toDoItem);
-        todoId = toDoItem.id;
-    }
-    todoId++;
 }
 
 
