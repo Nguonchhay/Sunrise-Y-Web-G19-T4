10b8830dad94fb757ecce89d60ac450243af3fd2
Touch Nguonchhay <nguonchhay.touch@gmail.com>
2024-05-24 18:43:03 +0700

    [TASK] React todo with render condition

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,25 +1,36 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
 
-  const todos = [
-    {
-      "id": 1,
-      "title": "Eating",
-      "status": "TO DO",
-      "dueDate": "2024-05-21 10:00:00"
-    },
-    {
-      "id": 1,
-      "title": "Eating",
-      "status": "TO DO",
-      "dueDate": "2024-05-21 10:00:00"
-    }
-  ];
+  const [todos, setToDos] = useState([]);
+  const [form, setForm] = useState('list');
 
-  return (
-    <div className="container">
-      <div className="col-6">
+  const renderToDoItem = (item) => {
+    return (
+      <tr key={item.id}>
+        <td>{item.id}</td>
+        <td>{item.title}</td>
+        <td>{item.status}</td>
+        <td>{item.dueDate}</td>
+        <td>
+          <div class="btn-group" role="group" aria-label="">
+            <button type="button" class="btn btn-primary">Edit</button>
+            <button type="button" class="btn btn-danger">Delete</button>
+          </div>
+        </td>
+      </tr>
+    )
+  }
+
+  const renderToDos = () => {
+    return todos.map(item => renderToDoItem(item) )
+  }
+
+
+  const renderToDoList = () => {
+    return (
+      <div>
         <h1 className="mt-5">ToDo List</h1>
         <table className="table tabled-border">
           <thead>
@@ -29,24 +40,41 @@ function App() {
               <th>Status</th>
               <th>Due Date</th>
               <th>
-                <button className="btn btn-primary">+ New</button>
+                <button onClick={() => setForm('create')} className="btn btn-primary">+ New</button>
               </th>
             </tr>
           </thead>
           <tbody>
-            { todos.map((item) => {
-              return (
-                <tr key={item.id}>
-                  <td>{item.id}</td>
-                  <td>{item.title}</td>
-                  <td>{item.status}</td>
-                  <td>{item.dueDate}</td>
-                  <td></td>
-                </tr>
-              )
-            }) }
+            { renderToDos() }
           </tbody>
         </table>
+      </div>
+    )
+  }
+
+  const renderToDoCreate = () => {
+    return (
+      <div>
+        New todo
+        <button onClick={() => setForm('list')} className="btn btn-primary">Back to list</button>
+      </div>
+    )
+  }
+
+  const renderContent = () => {
+    switch (form) {
+      case 'create':
+        return renderToDoCreate();
+        break;
+      default:
+        return renderToDoList();
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className="col-6">
+        { renderContent() }
       </div>
     </div>
   );
